0baf1273f608c277e942894cfb2e1727728079e2
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-12 16:17:06 +0500

    update

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     host: '127.0.0.1',
-    port: 8001
+    port: 8001,
+    disableHostCheck: true,
+    public: '127.0.0.1:8001'
   }
 })
