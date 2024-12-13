f2e93e45262748dfcc53492c2b926ed3fb6e68de
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-12-13 15:38:38 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -27,9 +27,9 @@ import { ClosingConfirmation, MainButton, useWebAppMainButton } from 'vue-tg';
 
 const { onMainButtonClicked } = useWebAppMainButton();
 
-this.$root.onMainButtonClicked(async () =>
-  await this.saveReport()
-);
+// onMainButtonClicked(async () =>
+//   await this.saveReport()
+// );
 
 export default {
   data() {
@@ -80,7 +80,9 @@ export default {
 
   },
   mounted() {
-    
+    this.$root.onMainButtonClicked(async () => {
+      await this.saveReport();
+    });
   },
   methods: {
     nextContent() {
